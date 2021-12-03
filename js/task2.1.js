73381c13001cb30a83b764a5ffabc52bf466a24b
D1rtySoul <95465193+D1rtySoul@users.noreply.github.com>
2021-12-03 12:44:50 +0600

    Add files via upload

---
+++ js/task2.1.js
@@ -0,0 +1,6 @@
+<SCRIPT LANGUAGE="JavaScript">
+//Выводится дата и время посещения страницы Now = new Date();
+document.write("Сегодня " + Now.getDate()+
+"-" + (Now.getMonth()+1) + "-" + Now.getFullYearO + ". Вы зашли на Web-страницу ровно в: " + Now.getHours() +
+":" + Now.getMinutes() + " и " + Now.getSeconds() +
+" секунд.") </SCRIPT>
